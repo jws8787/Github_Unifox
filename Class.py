e28658d170bc3cb3a5a9f9921fb9d0e9cd3f3b2f
jws8787 <jwsxzx@naver.com>
2017-08-29 20:44:13 +0900

    Class 과제 (다했다 기모띠)

---
+++ Class.py
@@ -0,0 +1,16 @@
+class StudentRecord:
+    students = list()
+    sort_id = list()
+    def append(self, student_id, name):
+        self.students.append({"student_id" : student_id, "name" : name})
+    def search(self, student_id):
+        for i in self.students:
+            self.n_id = i.get("student_id")
+            self.n_name = i.get("name")
+            if self.n_id == student_id:
+                print(self.n_name)
+    def name_list(self):
+        for i in self.students:
+            self.sort_id.append(i.get("student_id"))
+        self.sort_id.sort()
+        print(self.sort_id)
